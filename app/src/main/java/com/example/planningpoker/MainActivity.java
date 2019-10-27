6b385d7a531df6f0b4c9ba0b6fb9326b724df22f
Alida <alida_biro@yahoo.com>
2019-10-27 19:20:02 +0200

    User tabla

--- app/src/main/java/com/example/planningpoker/MainActivity.java
+++ app/src/main/java/com/example/planningpoker/MainActivity.java
@@ -3,26 +3,47 @@ package com.example.planningpoker;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.SignInMethodQueryResult;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.core.Tag;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
 
-    Button btn_register, btn_login;
-    EditText et_loginName, et_password;
-    String name, password;
-    DatabaseReference databaseUsers;
-    FirebaseAuth mFirebaseAuth;
-    FirebaseAuth.AuthStateListener mAuthStateListener;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button btn_register, btn_login;
+    private EditText et_loginEmail, et_password;
+    private String email, password;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private Boolean saveLogin;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference rootRef, itemsRef;
+    private ArrayList items_emails;
+    private ArrayList items_passwords;
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +52,18 @@ public class MainActivity extends AppCompatActivity {
 
         btn_register = findViewById(R.id.button_register);
         btn_login = findViewById(R.id.button_login);
-        et_loginName = findViewById(R.id.editText_loginName);
+        et_loginEmail = findViewById(R.id.editText_loginEmail);
         et_password = findViewById(R.id.editText_password);
-        databaseUsers = FirebaseDatabase.getInstance().getReference("users");
+        auth = FirebaseAuth.getInstance();
 
-        /*User u = new User();
-        if(databaseUsers.child("userName").equals(name))
-        {
 
-        }*/
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        rootRef = firebaseDatabase.getReference();
+        itemsRef = rootRef.child("Users");
+
+
+        btn_login.setOnClickListener(this);
+
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,22 +74,39 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        name = et_loginName.getText().toString().trim();
-        password = et_password.getText().toString().trim();
-        
-
-        btn_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = null;
-
-
-            }
-        });
     }
 
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference ref = database.getReference("registration");
+    public void onClick(View v) {
+        if (v == btn_login) {
+
+                auth.fetchSignInMethodsForEmail(et_loginEmail.getText().toString()).addOnCompleteListener(new OnCompleteListener<SignInMethodQueryResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<SignInMethodQueryResult> task) {
+                        if(task.getResult().getSignInMethods().isEmpty())
+                        {
+                            Toast.makeText(getApplicationContext(),"Invalid email adress",Toast.LENGTH_LONG).show();
+
+                        }
+                        else{
+
+                            Toast.makeText(getApplicationContext(),"Login Successful",Toast.LENGTH_LONG).show();
+                            doSomethingElse();
+
+                        }
+                    }
+                });
+
+            }
+    }
+
+    public void doSomethingElse() {
+        startActivity(new Intent(MainActivity.this, SessionActivity.class));
+        finish();
+    }
+
+   // FirebaseDatabase database = FirebaseDatabase.getInstance();
+   // DatabaseReference ref = database.getReference("registration");
 
 
 }
+
--- app/src/main/java/com/example/planningpoker/SessionActivity.java
+++ app/src/main/java/com/example/planningpoker/SessionActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Session_MasterActivity extends AppCompatActivity {
+public class SessionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_session__master);
+        setContentView(R.layout.activity_session);
     }
 }
